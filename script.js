1359c67ebab27ae01093d061f6dd9966fd36f060
DinoBarton <DinoBarton@protonmail.com>
2025-04-03 23:19:27 +0000

    HUGE IMPROVEMENTS

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-// Fetch GitHub profile information
-fetch('https://api.github.com/users/DinoBarton')
-.then(response => response.json())
-.then(data => {
-    const githubProfileInfo = document.getElementById('github-profile-info');
-    const profilePicture = document.getElementById('profile-picture');
-    profilePicture.src = data.avatar_url;
-    profilePicture.alt = "Profile Picture";
-    githubProfileInfo.innerHTML = `
-        <p>Username: ${data.login}</p>
-        <p>Name: ${data.name}</p>
-        <p>Bio: ${data.bio}</p>
-        <p>Location: ${data.location}</p>
-        <p>Public Repositories: ${data.public_repos}</p>
-    `;
-})
-.catch(error => {
-    console.error('Error fetching GitHub profile:', error);
-});
+const dynamicText = document.getElementById("dynamic-text");
+
+if (!dynamicText) {
+    console.error("Element with id 'dynamic-text' not found!");
+} else {
+    const phrases = ["I'm Dino Barton!", "I'm a web developer!", "I'm a tech enthusiast!", "Im an aspiring entrepreneur!"];
+    let currentPhraseIndex = 0;
+    let currentCharIndex = 0;
+    let isDeleting = false;
+
+    function typeEffect() {
+        const currentPhrase = phrases[currentPhraseIndex];
+        if (isDeleting) {
+            currentCharIndex--;
+            dynamicText.textContent = currentPhrase.substring(0, currentCharIndex);
+        } else {
+            currentCharIndex++;
+            dynamicText.textContent = currentPhrase.substring(0, currentCharIndex);
+        }
+
+        let typingSpeed = isDeleting ? 50 : 100;
+
+        if (!isDeleting && currentCharIndex === currentPhrase.length) {
+            typingSpeed = 2000;
+            isDeleting = true;
+        } else if (isDeleting && currentCharIndex === 0) {
+            isDeleting = false;
+            currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+        }
+
+        setTimeout(typeEffect, typingSpeed);
+    }
+
+    typeEffect();
+}
+
